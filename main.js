3cda0d8a8c505a985cf37e34ca59fc920f6a1793
JinHyun345 <kimjh0957@gmail.com>
2025-02-26 18:37:21 +0900

    Add files via upload
    POST 방식으로 title description 받고 data 폴더 안에 파일 생성하는것까지 완료, 수정기능 만드는 중 추가로 else if(pathname === "/updata"){이부분} 작성해야 함

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function HTMLtemplate(title, list, body){
+function HTMLtemplate(title, list, body, control){
     return `
         <!DOCTYPE html>
         <html lang="en">
@@ -12,9 +13,10 @@ function HTMLtemplate(title, list, body){
             <title>${title}</title>
         </head>
         <body>
-            <h1><a href = "/?id=EU">EU</a></h1>
+            <h1><a href = "/">EU</a></h1>
 
             ${list}
+            ${control}
 
             ${body}
         </body>
@@ -51,22 +53,64 @@ var app = http.createServer(function (request, response) {
                 var title = 'EU';
                 
                 fs.readFile(`data/${title}`, 'utf8', function(err, data){
-                    var template = HTMLtemplate(title, linklist(filelist), `<h3>${title}</h3><p>${data}</p>`);
+                    var Body = `<h3>${title}</h3><p>${data}</p>`;
+                    var control = `<a href="/add">add</a>`;
+                    var template = HTMLtemplate(title, linklist(filelist), Body, control);
                     response.writeHead(200);
                     response.end(template);
-                }) 
-            })
+                });
+            });
         }
         else {
             fs.readdir('./data', function(err, filelist) {
 
                 fs.readFile(`data/${title}`, 'utf8', function(err, data){
-                    var template = HTMLtemplate(title, linklist(filelist), `<h3>${title}</h3><p>${data}</p>`);
+                    var control = `<a href="/add">add</a> <a href="/update?id=${title}">update</a>`;
+                    var Body = `<h3>${title}</h3><p>${data}</p>`;
+                    var template = HTMLtemplate(title, linklist(filelist), Body, control);
                     response.writeHead(200);
                     response.end(template);
-                })
-            })
+                });
+            });
         } 
+    }
+    else if(pathname === "/add"){
+        fs.readdir('./data', function(err, filelist) {
+            var title = 'EU-add';
+            var template = HTMLtemplate(title, linklist(filelist), `
+            <form action = "http://localhost:3000/add_process" method = "post">
+                <p>
+                    <input type="text" name="title" placeholder ="title">
+                </p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>`, '');
+            response.writeHead(200);
+            response.end(template);         
+            
+        });
+    }
+    else if (pathname ==='/add_process'){
+
+        var body = '' ;
+        request.on('data', function(data){
+            body = body + data;
+        });
+        console.log(body);
+        request.on('end', function(){
+            var postpost = qs.parse(body);
+            var post = Object.assign({}, postpost);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                response.writeHead(302, {Location : `/?id=${title}`});
+                response.end(); 
+            });
+        }); 
     }
     else {
         response.writeHead(404);
